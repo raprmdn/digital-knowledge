1793d004ea1e870d5d2cdbee415f60e2e495a60f
Rafi1112 <rafiputraramadhan14@gmail.com>
2021-03-12 23:45:58 +0700

    command most popular

--- resources/views/frontend/layouts/side-widget.blade.php
+++ resources/views/frontend/layouts/side-widget.blade.php
@@ -4,26 +4,26 @@
             <div class="widget-header-1 position-relative mb-30">
                 <h5 class="mt-5 mb-30">Most popular</h5>
             </div>
-            <div class="post-block-list post-module-1">
-                <ul class="list-post">
-                    <li class="mb-30 wow fadeInUp animated">
-                        <div class="d-flex bg-white has-border p-25 hover-up transition-normal border-radius-5">
-                            <div class="post-content media-body">
-                                <h6 class="post-title mb-15 text-limit-2-row font-medium"><a href="single.html.htm">Spending Some Quality Time with Kids? It’s Possible</a></h6>
-                                <div class="entry-meta meta-1 float-left font-x-small text-uppercase">
-                                    <span class="post-on">05 August</span>
-                                    <span class="post-by has-dot">150 views</span>
-                                </div>
-                            </div>
-                            <div class="post-thumb post-thumb-80 d-flex ml-15 border-radius-5 img-hover-scale overflow-hidden">
-                                <a class="color-white" href="single.html.htm">
-                                    <img src="{{ asset('frontend/assets/imgs/news/thumb-6.jpg') }}" alt="">
-                                </a>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+{{--            <div class="post-block-list post-module-1">--}}
+{{--                <ul class="list-post">--}}
+{{--                    <li class="mb-30 wow fadeInUp animated">--}}
+{{--                        <div class="d-flex bg-white has-border p-25 hover-up transition-normal border-radius-5">--}}
+{{--                            <div class="post-content media-body">--}}
+{{--                                <h6 class="post-title mb-15 text-limit-2-row font-medium"><a href="single.html.htm">Spending Some Quality Time with Kids? It’s Possible</a></h6>--}}
+{{--                                <div class="entry-meta meta-1 float-left font-x-small text-uppercase">--}}
+{{--                                    <span class="post-on">05 August</span>--}}
+{{--                                    <span class="post-by has-dot">150 views</span>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <div class="post-thumb post-thumb-80 d-flex ml-15 border-radius-5 img-hover-scale overflow-hidden">--}}
+{{--                                <a class="color-white" href="single.html.htm">--}}
+{{--                                    <img src="{{ asset('frontend/assets/imgs/news/thumb-6.jpg') }}" alt="">--}}
+{{--                                </a>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+{{--                    </li>--}}
+{{--                </ul>--}}
+{{--            </div>--}}
         </div>
     </div>
 </div>
